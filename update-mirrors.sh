85f201206e99ad18cd36ea766a2eba2ce2695bae
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2023-07-12 03:01:34 +0800

    style: update line spaces

--- update-mirrors.sh
+++ update-mirrors.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+
 mirror_url="https://archlinux.org/mirrorlist/?country=SG&protocol=https&ip_version=4&use_mirror_status=on"
 curl -s $mirror_url | cut -c2- | sudo tee /etc/pacman.d/mirrorlist
-
